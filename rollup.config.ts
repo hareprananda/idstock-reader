d94182d252adc82d4e293d2ed49e5d15e92f7cd6
Hare Prananda <hareprananda9@gmail.com>
2022-08-02 08:33:53 +0800

    initial commit

---
+++ rollup.config.ts
@@ -0,0 +1,9 @@
+import typescript from "rollup-plugin-typescript2";
+import ttypescript from "ttypescript";
+import fs from "fs";
+import path from "path";
+import { terser } from "rollup-plugin-terser";
+
+const options = [{}];
+
+export default options;
