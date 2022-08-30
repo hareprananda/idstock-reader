b14d57103562134d20a1e256564cb17ca2f8a95c
Hare Prananda <hareprananda9@gmail.com>
2022-08-30 08:17:18 +0800

    Add test case

---
+++ __test__/Reader.test.ts
@@ -0,0 +1,76 @@
+import path from "path";
+import ReadFinancial from "../src";
+import fs from "fs";
+
+describe("Reader testing", () => {
+  for (const filePath of fs.readdirSync(
+    path.resolve("__test__/financialReport")
+  )) {
+    const financeInsEn = new ReadFinancial({
+      path: path.resolve(`__test__/financialReport/${filePath}`),
+      language: "en",
+    });
+
+    const financeInsId = new ReadFinancial({
+      path: path.resolve(`__test__/financialReport/${filePath}`),
+      language: "id",
+    });
+
+    const fileName = filePath.match(/^[\d\w]+/g)?.[0] as string;
+
+    it(`Sheets number of ${fileName}`, async () => {
+      const numberOfSheets = await financeInsEn.getSheets();
+      expect(numberOfSheets.length).toBe(9);
+    });
+
+    it(`General data of ${fileName}`, async () => {
+      const [enGeneral, idGeneral] = [
+        await financeInsEn.getGeneral(),
+        await financeInsId.getGeneral(),
+      ];
+      const idKeyLength = Object.keys(idGeneral).length;
+      expect(idKeyLength).toBeGreaterThan(10);
+      expect(idKeyLength).toBe(Object.keys(enGeneral).length);
+    });
+
+    it(`Balance data of ${fileName}`, async () => {
+      const [enBalance, idBalance] = [
+        await financeInsEn.getBalance(),
+        await financeInsId.getBalance(),
+      ];
+      const idKeyLength = Object.keys(idBalance).length;
+      expect(idKeyLength).toBeGreaterThan(30);
+      expect(idKeyLength).toBe(Object.keys(enBalance).length);
+    });
+
+    it(`Income data of ${fileName}`, async () => {
+      const [enIncome, idIncome] = [
+        await financeInsEn.getIncome(),
+        await financeInsId.getIncome(),
+      ];
+      const idKeyLength = Object.keys(idIncome).length;
+      expect(idKeyLength).toBeGreaterThan(15);
+      expect(idKeyLength).toBe(Object.keys(enIncome).length);
+    });
+
+    it(`Cashflow data of ${fileName}`, async () => {
+      const [enCashflow, idCashflow] = [
+        await financeInsEn.getCashflow(),
+        await financeInsId.getCashflow(),
+      ];
+      const idKeyLength = Object.keys(idCashflow).length;
+      expect(idKeyLength).toBeGreaterThan(15);
+      expect(idKeyLength).toBe(Object.keys(enCashflow).length);
+    });
+
+    it(`Change Statement data of ${fileName}`, async () => {
+      const [enGeneral, idGeneral] = [
+        await financeInsEn.getChangeStatement(),
+        await financeInsId.getChangeStatement(),
+      ];
+      const idKeyLength = Object.keys(idGeneral).length;
+      expect(idGeneral).toBeInstanceOf(Object);
+      expect(idKeyLength).toBe(Object.keys(enGeneral).length);
+    });
+  }
+});
